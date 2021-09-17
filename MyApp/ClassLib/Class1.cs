a6b5a130c472c244ad768627ddd1c190b74394c3
jkof <jkof@itu.dk>
2021-09-17 13:01:29 +0200

    Add Student class

--- MyApp/ClassLib/Class1.cs
+++ MyApp/ClassLib/Class1.cs
@@ -4,6 +4,20 @@ namespace ClassLib
 {
     public class Student
     {
-        
+        int id;
+        string GivenName;
+        string Surname;
+        readonly Status Status;
+        DateTime StartDate;
+        DateTime EndDate;
+        DateTime GraduationDate;
+    }
+
+    public enum Status
+    {
+        New,
+        Active,
+        Dropout,
+        Graduated
     }
 }
