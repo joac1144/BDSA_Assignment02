6ec091af1319a61c0181634b9e680e852cea56a3
vibr <vibr@itu.dk>
2021-09-17 13:51:02 +0200

    Created and tested student

--- MyApp/ClassLib.Tests/UnitTest1.cs
+++ MyApp/ClassLib.Tests/UnitTest1.cs
@@ -1,13 +1,23 @@
 using System;
 using Xunit;
+using System.Globalization;
 
 namespace ClassLib.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Status_is_only_readable()
         {
+            //Arrannge
+            Student student = new Student(1, "Joachim", "Koefoed", new DateTime(2020, 09, 16), new DateTime(2021, 09, 16), new DateTime(2022, 09, 16));
+
+            //Act
+            var output = student.Status;
+
+            //Assert
+            Assert.Equal(Status.Dropout, output);
+            
 
         }
     }
--- MyApp/ClassLib/Class1.cs
+++ MyApp/ClassLib/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ClassLib
 {
@@ -7,10 +8,35 @@ namespace ClassLib
         int id;
         string GivenName;
         string Surname;
-        readonly Status Status;
+
+        public Status Status{
+            get {
+                if(DateTime.Compare(StartDate, DateTime.Now) > 30){
+                    return Status.New;
+                }else if(EndDate < GraduationDate){
+                    return Status.Dropout;
+                }else if(EndDate.Equals(GraduationDate)){
+                    return Status.Graduated;
+                }else{
+                    return Status.Active;
+                }
+            }
+
+        }
+
         DateTime StartDate;
         DateTime EndDate;
         DateTime GraduationDate;
+
+        public Student(int id, string GivenName, string Surname, DateTime StartDate, DateTime EndDate, DateTime GraduationDate){
+            this.id = id;
+            this.GivenName = GivenName;
+            this.Surname = Surname;
+            this.StartDate = StartDate;
+            this.EndDate = EndDate;
+            this.GraduationDate = GraduationDate;
+        }
+
     }
 
     public enum Status
@@ -21,3 +47,9 @@ namespace ClassLib
         Graduated
     }
 }
+
+
+// Active = between startdate and 3 months after  
+// Active = between end/grad and start
+// Dropout = Enddate before grad date
+// Grad = Enddate = graduationdate 
