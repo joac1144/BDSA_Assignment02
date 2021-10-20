7bfb525dbe1a0750032700b8df68192a31f40e1d
nissemand243 <47781194+nissemand243@users.noreply.github.com>
2021-10-20 16:08:48 +0200

    Update UnitTest1.cs

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -2,23 +2,14 @@ using System;
 using Xunit;
 using System.Globalization;
 
-namespace ClassLib.Tests
+namespace MyApp.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Status_is_only_readable()
+        public void Test1()
         {
-            //Arrannge
-            Student student = new Student(1, "Joachim", "Koefoed", new DateTime(2020, 09, 16), new DateTime(2021, 09, 16), new DateTime(2022, 09, 16));
-
-            //Act
-            var output = student.Status;
-
-            //Assert
-            Assert.Equal(Status.Dropout, output);
-            
-
+           
         }
     }
 }
